a314e34a6481e8fbbe76633410c01a750bb9363e
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-09-28 22:06:00 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -19,16 +19,60 @@ const BMIData = [
 const size = document.getElementById("size");
 const numberresult = document.querySelector(".number-result");
 const testresult = document.querySelector(".text-result");
-let centimeter, kilogram, imc;
+const sizeerror = document.querySelector(".size-error");
+const weighterror = document.querySelector(".weight-error");
+const textresult = document.querySelector(".text-result");
+let centimeter = 0;
+let kilogram = 0;
+let imc = 0;
 
 const calculate = () => {
-  if (centimeter > 0 && kilogram > 0) {
+  if (centimeter >= 2 && centimeter <= 5 && kilogram >= 20 && kilogram <= 800) {
     imc = (kilogram / centimeter).toFixed(1);
     numberresult.textContent = imc;
     size.value = "";
     weight.value = "";
+    sizeerror.textContent = "";
+    weighterror.textContent = "";
+  }
+
+  if (centimeter == 0) {
+    sizeerror.textContent = "Veuillez rentrer un chiffre ";
+    size.value = "";
+    weight.value = "";
+  }
+  if (kilogram == 0) {
+    weighterror.textContent = "Veuillez rentrer un chiffre ";
+    size.value = "";
+    weight.value = "";
+  }
+  if (centimeter <= 2 || centimeter >= 6) {
+    sizeerror.textContent = "Veuillez rentrer un chiffre cohérent";
+    size.value = "";
+    weight.value = "";
+  }
+
+  if (kilogram <= 20 && kilogram > 900) {
+    weighterror.textContent = "Veuillez rentrer un chiffre cohérent";
+    size.value = "";
+    weight.value = "";
   }
 };
+
+/*
+const result = () => {
+   (imc <= 18.5) {
+    const bname = BMIData[0].name;
+    textresult.textContent = bname;
+  } else if (imc >= 18.5 || imc <= 25) {
+    const bname2 = BMIData[1].name;
+    textresult.textContent = bname2;
+  } else if (imc >= 25 || imc <= 30) {
+    const bname3 = BMIData[2].name;
+    textresult.textContent = bname3;
+  }
+};
+*/
 
 //size
 size.addEventListener("change", (e) => {
@@ -46,6 +90,7 @@ weight.addEventListener("change", (e) => {
 //calculate IMC at click
 calculateImc.addEventListener("click", (e) => {
   calculate();
+  result();
 });
 
 //result from table
